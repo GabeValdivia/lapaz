e57f6729524690f4fb98c966d89f6fead23ff0c6
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 21:38:14 -0800

    Add Google Fonts to functions file, add to CSS

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -2,6 +2,7 @@
 
 function lapizzera_styles() {
     // Adding stylesheests
+    wp_register_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900&display=swap', array(), '1.0.0');
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize', array(), '8.0.1');
     wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
@@ -10,6 +11,7 @@ function lapizzera_styles() {
     // Enqueue the Style
     wp_enqueue_style('normalize');
     wp_enqueue_style('fontawesome');
+    wp_enqueue_style('googlefont');
     wp_enqueue_style('style');
     
     wp_register_script('script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
