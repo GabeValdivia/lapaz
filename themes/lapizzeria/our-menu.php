bbc11d2783c269c00dd9361033723b83fabcab23
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-19 14:50:34 -0800

    Add query to php query to pull in 'others' in specialties and link to respective post

--- themes/lapizzeria/our-menu.php
+++ themes/lapizzeria/our-menu.php
@@ -23,7 +23,7 @@
                 <?php the_content(); ?>
             </main>
         </div>
-
+        <!-- Specialties Pizza  -->
         <class class="our-specialties">
             <h3 class="primary-text">Pizzas</h3>
             <div class="container-grid">
@@ -32,18 +32,43 @@
                         'post_type' => 'specialties',
                         'posts_per_page' => 10,
                         'orderby' => 'title',
-                        'order' => 'ASC'
+                        'order' => 'ASC',
+                        'category_name' => 'pizzas'
                     );
-
                     $pizzas = new WP_Query($args);
                     while($pizzas->have_posts()): $pizzas->the_post(); ?>
-
-                    <h2><?php the_title(); ?></h2>
-                        
-                        
+                    <div class="columns2-4">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('specialties'); ?>
+                            <h4><?php the_title(); ?> <span>$<?php the_field('price'); ?></span></h4>
+                            <?php the_content(); ?>
+                        </a>
+                    </div>                        
                     <?php endwhile; wp_reset_postdata(); ?>
             </div>
         </class>
+        <!-- Specialties Others -->
+        <h3 class="primary-text">Others</h3>
+        <div class="container-grid">
+            <?php                 
+                $args = array(
+                    'post_type' => 'specialties',
+                    'posts_per_page' => 10,
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                    'category_name' => 'others'
+                );
+                $pizzas = new WP_Query($args);
+                while($pizzas->have_posts()): $pizzas->the_post(); ?>
+                <div class="columns2-4">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('specialties'); ?>
+                        <h4><?php the_title(); ?> <span>$<?php the_field('price'); ?></span></h4>
+                        <?php the_content(); ?>
+                    </a>
+                </div>                        
+                <?php endwhile; wp_reset_postdata(); ?>
+        </div>
         
 
         
