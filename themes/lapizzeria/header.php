4b862834b8a9d50c0ab028ac7366b5cd8c94b35b
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 17:46:17 -0800

    Add header global CSS styles, set container width for site

--- themes/lapizzeria/header.php
+++ themes/lapizzeria/header.php
@@ -10,30 +10,32 @@
 <body>
 
 <header class="site-header">
-    <div class="logo">
-        <a href="<?php echo esc_url( home_url('/') ); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" class="logo-image" />
-        </a>
-    </div><!-- Logo -->
-    <div class="header-information">
-        <div class="socials">
-        <?php 
-            $args = array(
-                'theme_location' => 'social-menu',
-                'container'      => 'nav',
-                'container_class' => 'socials',
-                'container_id'   => 'socials',
-                'link_before'    => '<span class="sr-text">',
-                'link_after'     => '</span>'
-            );
-            wp_nav_menu($args);
-        ?>
-        </div><!-- Socials -->
-        <div class="address">
-            <p>8179 Bay Avenue Mountain View, CA 94043</p>
-            <p>Phone Number: +1-92-456-7890</p>
-        </div>
-    </div><!-- Header Information -->
+    <div class="container">
+        <div class="logo">
+            <a href="<?php echo esc_url( home_url('/') ); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" class="logo-image" />
+            </a>
+        </div><!-- Logo -->
+        <div class="header-information">
+            <div class="socials">
+            <?php 
+                $args = array(
+                    'theme_location' => 'social-menu',
+                    'container'      => 'nav',
+                    'container_class' => 'socials',
+                    'container_id'   => 'socials',
+                    'link_before'    => '<span class="sr-text">',
+                    'link_after'     => '</span>'
+                );
+                wp_nav_menu($args);
+            ?>
+            </div><!-- Socials -->
+            <div class="address">
+                <p>8179 Bay Avenue Mountain View, CA 94043</p>
+                <p>Phone Number: +1-92-456-7890</p>
+            </div><!-- Container -->
+        </div><!-- Header Information -->
+    </div>
 </header>
 <div class="main-menu">
     <div class="navigation">
