0cc3bf98316ce1128e7b088ba85850b13547b251
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-19 00:20:23 -0800

    Add HTML for box-information section

---
+++ themes/lapizzeria/page-about-us.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+    <!-- Hero Section  -->
+    <?php while(have_posts()): the_post(); ?>
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="hero-content">
+                <hero-text>
+                <h2><?php the_title(); ?></h2>
+                </hero-text>
+            </div>
+        </div>
+    <!-- Main Content Section -->
+        <div class="main-content container">
+            <main class="text-center content-text">
+                <?php the_content(); ?>
+            </main>
+        </div>
+    <!-- Info Box Area -->
+    <div class="box-information container clear">
+        <div class="box">
+            <div class="content-box">
+
+            </div>
+        </div>
+    </div>
+        
+
+        
+        
+
+        <?php endwhile; ?>
+
+<?php get_footer(); ?>
