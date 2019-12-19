2ab9a83ea34c6728d7632695d22bd7bd4f873e3f
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 17:07:56 -0800

    Add wp_nav_menu function with

--- themes/lapizzeria/header.php
+++ themes/lapizzeria/header.php
@@ -16,3 +16,18 @@
         </a>
     </div>
 </header>
+<div class="main-menu">
+    <div class="navigation">
+        <?php 
+            $args = array(
+                'theme_location' => 'header-menu',
+                'container'      => 'nav',
+                'container_class' => 'site-nav'
+            );
+            wp_nav_menu($args);
+        ?>
+        <nav class="site-nav">
+
+        </nav>
+    </div>
+</div>
