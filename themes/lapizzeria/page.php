2e469cfb357503fdb62e801a477b6e3a165ba6bd
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 23:48:18 -0800

    Add post thumbnail as background to .hero div using get_the_post_thumbnail_url() function on page.php

--- themes/lapizzeria/page.php
+++ themes/lapizzeria/page.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
 
     <?php while(have_posts()): the_post(); ?>
-        <div class="hero">
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
             <div class="hero-content">
                 <hero-text>
-                <?php the_post_thumbnail(); ?>
                 <h2><?php the_title(); ?></h2>
                 </hero-text>
             </div>
