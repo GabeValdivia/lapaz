22f679751d325b9abaf7a7ddbca27c566a3db840
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 18:18:31 -0800

    Add font awesome to files and functions.php

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -3,11 +3,13 @@
 function lapizzera_styles() {
     // Adding stylesheests
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize', array(), '8.0.1');
+    wp_register_style('fontawesome', get_template_directory_uri() . '/css/fontawesome.min.css', array(), '5.12.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
     
     
     // Enqueue the Style
     wp_enqueue_style('normalize');
+    wp_enqueue_style('fontawesome');
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'lapizzera_styles');
