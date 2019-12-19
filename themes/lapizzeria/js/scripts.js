bad13d6ce739378a49ef10549e26e52a64598a9f
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 21:26:46 -0800

    Add JQuery function to show/hide desktop menu based on window breakpoint of min 768

--- themes/lapizzeria/js/scripts.js
+++ themes/lapizzeria/js/scripts.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($){
-    console.log("Hello from new scripts file!")
+    $('.mobile-menu a').on('click', function(){
+        $('nav.site-nav').toggle('slow');
+    });
+
+    var breakpoint = 768;
+    $(window).resize(function(){
+        if($(document).width() >= breakpoint) {
+            $('nav.site-nav').show();
+        } else {
+            $('nav.site-nav').hide();
+        }
+    });
 });
