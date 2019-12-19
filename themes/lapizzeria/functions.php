7b7e5427d86ffd30bbeb40f22401b86dd47de0dc
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-18 16:16:02 -0800

    Register style.css and normalize.css in function.phhp

---
+++ themes/lapizzeria/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function lapizzera_styles() {
+    // Adding stylesheests
+    wp_register_style('normalize', get_template_directory_uri() . '/css/normalize', array(), '8.0.1');
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
+    
+    
+    // Enqueue the Style
+    wp_enqueue_style('normalize');
+    wp_enqueue_style('style');
+}
+add_action('wp_enqueue_scripts', 'lapizzera_styles');
+
