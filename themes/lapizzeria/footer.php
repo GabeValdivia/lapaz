731f781704bbe8d10ecbc02915fd4cc2a89fc6f3
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-19 11:38:07 -0800

    Add 'header-menu' via  wp_nav_menu function to footer, add address, phone and copyright HTML

--- themes/lapizzeria/footer.php
+++ themes/lapizzeria/footer.php
@@ -1,4 +1,23 @@
     
+    
+    <footer>
+        <?php
+            $args = array(
+                'theme_location' => 'header-menu',
+                'container' => 'nav',
+                'after' => '<span class="separator> | </span>"'
+            );
+            wp_nav_menu($args);
+        ?>
+
+        <div class="location">
+            <p>8179 Bay Avenue, Mountain View, CA 97043</p>
+            <p>Phone Number: +1-92-456-7890</p>
+
+            <p class="copyright">All rights reserved <?php echo date('Y'); ?></p>
+        </div>
+    </footer>
+    
     <?php wp_footer(); ?>
     </body>
 </html>
