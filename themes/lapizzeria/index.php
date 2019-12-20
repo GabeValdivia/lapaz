f286811a32c7ddb38a699e861ce9c4ac5f91fb93
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-19 18:19:00 -0800

    Add needed variables for  and  to index.php in order to pull in the blog page title and image

--- themes/lapizzeria/index.php
+++ themes/lapizzeria/index.php
@@ -1,4 +1,33 @@
-<?php 
-    echo "<h1>hello from index.php!</h1>";
+<?php get_header(); ?>
 
-?>
+    <?php 
+        $blog_page = get_option('page_for_posts');
+        $image = get_post_thumbnail_id($blog_page);
+        $image = wp_get_attachment_image_src($image, 'full');
+    
+    ?>
+
+    
+        <div class="hero" style="background-image:url(<?php echo $image[0]; ?>);">
+            <div class="hero-content">
+                <hero-text>
+                <h2><?php echo get_the_title($blog_page); ?></h2>
+                </hero-text>
+            </div>
+        </div>
+
+        <div class="main-content container">
+            <main class="text-center content-text">
+            <?php while(have_posts()): the_post(); ?>
+
+            <?php endwhile; ?>
+            </main>
+        </div>
+        
+
+        
+        
+
+        
+
+<?php get_footer(); ?>
