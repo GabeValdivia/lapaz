3ac9950b29a336b98ab758dd0d31ca3196e98f2a
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-21 17:29:45 -0800

    Style table on Reservatons Dashboard to match WP defaults, write  functon to iterate read SQL Query needed to read and echo id, name, phone, email, and message. Check to see that contact form enteries are printed on Options>Reservations

--- themes/lapizzeria/inc/options.php
+++ themes/lapizzeria/inc/options.php
@@ -9,9 +9,43 @@ add_action('admin_menu','lapizzeria_options');
 
 function lapizzeria_adjustments(){
     //Rest of the code
-
-function lapizzeria_reservations() {
-    //Rest of the code
 }
+
+function lapizzeria_reservations() { ?>
+    <div class="wrap">
+        <h1>Reservations</h1>
+        <table class="wp-list-table widefat striped">
+            <thead>
+                <tr>
+                    <th class="manage-column">ID</th>
+                    <th class="manage-column">Name</th>
+                    <th class="manage-column">Date of Reservation</th>
+                    <th class="manage-column">Email</th>
+                    <th class="manage-column">Phone Number</th>
+                    <th class="manage-column">Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    global $wpdb;
+                    $table = $wpdb->prefix . 'reservations';
+                    $reservations = $wpdb->get_results("SELECT * FROM $table", ARRAY_A);
+                    foreach($reservations as $reservation): ?>
+
+                        <tr>
+                            <td><?php echo $reservation['id'] ?></td>
+                            <td><?php echo $reservation['name'] ?></td>
+                            <td><?php echo $reservation['date'] ?></td>
+                            <td><?php echo $reservation['email'] ?></td>
+                            <td><?php echo $reservation['phone'] ?></td>
+                            <td><?php echo $reservation['message'] ?></td>
+                        </tr>
+
+                    <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+<?php }
 
 ?>
