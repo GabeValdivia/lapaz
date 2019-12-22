0b077da925d0f6e70c3601dbd6bafd98681734db
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-21 17:44:00 -0800

    Replace front-page code with page.php code, begin to modify to match design. Pull in site tagline as hero text. Add button to link to /contact-us/

--- themes/lapizzeria/front-page.php
+++ themes/lapizzeria/front-page.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
 
-    <h1>Hello from front-page.php</h1>
+    <?php while(have_posts()): the_post(); ?>
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="hero-content">
+                <hero-text>
+                <h2><?php echo esc_html( get_option('blogdescription')); ?></h2>
+                <?php the_content(); ?>
+                <?php $url = get_page_by_title('Contact Us'); ?>
+                <a class="button secondary" href="<?php echo get_permalink($url->ID); ?>">More Info</a>
+                </hero-text>
+            </div>
+        </div>
+
+        <div class="main-content container">
+            <main class="text-center content-text clear">
+                
+            </main>
+        </div>
+        
+
+        
+        
+
+        <?php endwhile; ?>
 
 <?php get_footer(); ?>
