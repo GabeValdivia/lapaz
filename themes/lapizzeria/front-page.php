9339514881110123b10ddd4abd684dc20f78670c
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-21 18:06:58 -0800

    Correct misnamed herotext div to div. Add styles to align hero text and hero height. Check all styles look good on mobile

--- themes/lapizzeria/front-page.php
+++ themes/lapizzeria/front-page.php
@@ -3,12 +3,12 @@
     <?php while(have_posts()): the_post(); ?>
         <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
             <div class="hero-content">
-                <hero-text>
+                <div class="hero-text">
                 <h2><?php echo esc_html( get_option('blogdescription')); ?></h2>
                 <?php the_content(); ?>
                 <?php $url = get_page_by_title('Contact Us'); ?>
                 <a class="button secondary" href="<?php echo get_permalink($url->ID); ?>">More Info</a>
-                </hero-text>
+                </div>
             </div>
         </div>
 
