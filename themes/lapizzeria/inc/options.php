e82f4b3aafc8534158e8484f6823d752e32a8985
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-21 16:41:06 -0800

    Add add_submenu_page function to lapizzeria_options(), test on WP Dashboard

--- themes/lapizzeria/inc/options.php
+++ themes/lapizzeria/inc/options.php
@@ -2,11 +2,16 @@
 
 function lapizzeria_options() {
     add_menu_page( 'La Pizzeria', 'La Pizzeria Options', 'administrator', 'lapizzeria_adjustments', 'lapizzeria_adjustments', '', 20);
+
+    add_submenu_page('lapizzeria_adjustments', 'Reservations', 'Reservations', 'administrator', 'lapizzeria_reservations', 'lapizzeria_reservations');
 }
 add_action('admin_menu','lapizzeria_options');
 
 function lapizzeria_adjustments(){
-    echo "Hello from lapizzeria_adjustments";
+    //Rest of the code
+
+function lapizzeria_reservations() {
+    //Rest of the code
 }
 
 ?>
